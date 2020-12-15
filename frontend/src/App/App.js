5b537910b241c26012b6426d944cf0538ec79017
Ilva Lamberte <lamberteilva@gmail.com>
2020-12-15 20:41:56 +0100

    added mobx dependencies and started frontend components in src

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,13 +1,30 @@
 
 import React from 'react';
-import UserStore from './stores/UserStore';
-import LoginForm from './LoginForm';
-import InputField from './InputField';
-import SubmitButton from './SubmitButton';
+import UserStore from '../stores/UserStore';
+import LoginForm from '../components/LoginForm';
+import InputField from '../components/InputField';
+import SubmitButton from '../components/SubmitButton';
 
 class App extends React.Component {
 
   async componentDidMount() {
+
+    try {
+
+        let res = await fetch('./isLoggedIn',  {
+            method: 'post',
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json'
+            }
+
+        })
+
+    }
+
+    catch(e) {
+
+    }
     
   }
 
