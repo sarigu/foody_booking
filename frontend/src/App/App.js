7d83ca83489c7cad004eba9c992b007ac64350f6
Ilva Lamberte <lamberteilva@gmail.com>
2020-12-15 18:37:55 +0100

    started components in react

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,12 +1,21 @@
 
 import React from 'react';
+import UserStore from './stores/UserStore';
+import LoginForm from './LoginForm';
+import InputField from './InputField';
+import SubmitButton from './SubmitButton';
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    
+  }
 
   render() {
   return (
     <div className="app">
       <h1>Hello</h1>
+    
     </div>
   );
 }}
---
+++ frontend/src/components/InputField.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+class App extends React.Component {
+
+  render() {
+  return (
+    <div className="inputField">
+      <h1>Hello</h1>
+    
+    </div>
+  );
+}}
+
+export default InputField;
