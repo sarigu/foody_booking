5bce7fd2caabed54b296a55049012ed41781f32a
Ilva Lamberte <lamberteilva@gmail.com>
2020-12-15 17:49:55 +0100

    mobx - created userstore

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+
 import React from 'react';
 
-function App() {
+class App extends React.Component {
+
+  render() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Hello</h1>
     </div>
   );
-}
+}}
 
 export default App;
