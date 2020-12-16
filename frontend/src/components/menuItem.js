9ca11432ad3a7c37b697f0214e2f0b1c6fa07ed3
Sari <sariguci@Saris-MacBook-Pro.local>
2020-12-16 01:08:40 +0100

    feat/ added menu overview

---
+++ frontend/src/components/menuItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+export default class MenuItem extends Component {
+
+    render() {
+
+        const { Item, Price, Description } = this.props.item;
+
+        return (
+            <div >
+                <div><h2> {Item}</h2></div>
+                <div><span className="boldText">Price:</span> {Price}</div>
+                <div><span className="boldText">Description:</span> {Description}</div>
+            </div>
+        );
+    }
+}
+
+MenuItem.propTypes = {
+    item: PropTypes.object
+};
