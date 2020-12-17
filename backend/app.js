b1e3171af7cb0b3478d3f3ac3cd85d2cdce8d6b8
Suman Thapa Magar <suma0449@stud.kea.dk>
2020-12-17 15:50:54 +0100

    Added CRUD funcationality for table/seat node js

--- backend/app.js
+++ backend/app.js
@@ -71,6 +71,56 @@ app.post("/restaurantdetails", async (req, res) => {
     });
 });
 
+//get Table
+app.get("/seat", (req, res) => {
+    con.query("SELECT * FROM Seat", function (err, result) {
+        if (err) throw err;
+        data = JSON.parse(JSON.stringify(result));
+        return res.send({ data: data });
+    });
+});
+
+// Add a Table seat
+app.post("/seatadd", (req, res) => {
+    const sqlInsert = "INSERT INTO `seat` (`SeatID`, `SeatName`, `SeatCapacity`, `SeatStatus`) VALUES (NULL, 'Lounge 5', '25', '0');";
+    con.query(sqlInsert, function (err, result) {
+        res.send("Data Instrted");
+    });
+});
+
+// Delete a Table seat
+app.get("/deleteseat", (req, res) => {
+    // for dynamic
+    // const sqlQuery = ("DELETE FROM `seat` WHERE `seat`.`SeatID` = ?", req.query.id);
+    const sqlQuery = "DELETE FROM `seat` WHERE `seat`.`SeatID` = 5";
+    con.query(sqlQuery, function (err, result) {
+        res.redirect('/seat');
+    });
+});
+
+// Getting id for editing a Table Seat
+app.get("/edit", (req, res) => {
+    // const sqlQuery = ("SELECT * FROM Seat WHERE SeatID = 1", req.query.id);
+    const sqlQuery = "SELECT * FROM Seat WHERE SeatID = 1";
+    con.query(sqlQuery, function (err, result) {
+        if (err) throw err;
+        res.redirect('/edit');
+    });
+});
+
+// Update a Table Seat
+app.post("/edit", (req, res) => {
+    // var param = [
+    //     req.body,       //data for update
+    //     req.query.id    //condition for update
+    // ]
+    // const sqlQuery = ("UPDATE `seat` SET ? WHERE `seat`.`SeatID` = ?", param);
+    const sqlQuery = "UPDATE `seat` SET `SeatName` = 'VIP Lounge 1' WHERE `seat`.`SeatID` = 1";
+    con.query(sqlQuery, function (err, result) {
+        if (err) throw err;
+        res.redirect('/seat');
+    });
+});
 
 //  RUN SERVER
 
