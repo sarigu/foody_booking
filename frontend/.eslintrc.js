c7dbb583868cfbd71eef5552e18930e155f19433
Sari <sari0403@stud.kea.dk>
2021-01-03 16:48:06 +0000

    clean/removed overwritten import rules in eslintsrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -48,8 +48,5 @@ module.exports = {
         "prefer-template": "warn",
         "react/jsx-one-expression": "off",
         "react/jsx-one-expression-per-line": "off",
-        "import/extensions": "off",
-        "import/no-unresolved": "off",
-
     }
 };
