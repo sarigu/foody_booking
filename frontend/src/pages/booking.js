b90a24d454c3ea67af6e9186b359cbd430628377
Sari <sariguci@Saris-MacBook-Pro.local>
2021-01-03 17:23:01 +0100

    fix/removed duplicate, unlinted files + linted again

--- frontend/src/pages/booking.js
+++ frontend/src/pages/booking.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import SeatBooking from '../components/seatbooking';
+import SeatBooking from '../components/SeatBooking';
 
 export default class Booking extends React.Component {
-    constructor() {
-        super();
-        this.state = { booking: [] };
-    }
+  /* constructor() {
+         super();
+         this.state = { booking: [] };
+     } */
 
     componentDidMount = async () => {
-        try {
-            const response = await fetch("http://localhost:8000/booking");
-            const data = await response.json();
-            this.setState({ bookings: data.data });
-        } catch (e) {
-            console.log(e);
-        }
+      try {
+        const response = await fetch('http://localhost:8000/booking');
+        const data = await response.json();
+        this.setState({ bookings: data.data });
+      } catch (e) {
+        console.log(e);
+      }
     }
 
     render() {
-        return (
-            <div>
-                <h1>Bookings</h1>
-                {this.state.bookings && this.state.bookings.map((item, index) => (
-                    <SeatBooking key={"item" + index} item={item} />
-                ))}
-            </div>
-        );
+      return (
+        <div>
+          <h1>Bookings</h1>
+          {this.state.bookings && this.state.bookings.map((item, index) => (
+            <SeatBooking key={`item${index}`} item={item} />
+          ))}
+        </div>
+      );
     }
 }
--- frontend/src/pages/seat.js
+++ frontend/src/pages/seat.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import MenuItem from "../components/menuItem"
-import SeatPlan from '../components/seatplan';
+import SeatPlan from '../components/Seatplan';
 
 export default class Seat extends React.Component {
-    constructor() {
+  /* constructor() {
         super();
         this.state = { seatPlans: [] };
-    }
+    } */
 
     componentDidMount = async () => {
-        try {
-            const response = await fetch("http://localhost:8000/seat");
-            const data = await response.json();
-            this.setState({ seatPlans: data.data });
-        } catch (e) {
-            console.log(e);
-        }
+      try {
+        const response = await fetch('http://localhost:8000/seat');
+        const data = await response.json();
+        this.setState({ seatPlans: data.data });
+      } catch (e) {
+        console.log(e);
+      }
     }
 
     render() {
-        return (
-            <div>
-                <h1>Seat</h1>
-                {this.state.seatPlans && this.state.seatPlans.map((item, index) => (
-                    <SeatPlan key={"item" + index} item={item} />
-                ))}
-            </div>
-        );
+      return (
+        <div>
+          <h1>Seat</h1>
+          {this.state.seatPlans && this.state.seatPlans.map((item, index) => (
+            <SeatPlan key={`item${index}`} item={item} />
+          ))}
+        </div>
+      );
     }
 }
