17b28acab6feb9314578af3908742fb33253454c
Sari <sariguci@Saris-MacBook-Pro.local>
2021-01-05 14:22:52 +0100

    test/added label input tests

--- frontend/src/App/App.test.js
+++ frontend/src/App/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
 
+test('should return input for label on login page', () => {
+    const { getByLabelText } = render(<App />);
+    expect(getByLabelText('Email')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
 });
