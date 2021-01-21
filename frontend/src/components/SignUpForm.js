82420f93a58aa9083f0b1fe741666c55aa112cf3
Sari <sariguci@Saris-MacBook-Pro.local>
2021-01-21 05:27:36 +0100

    added required for signup fields

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -5,11 +5,11 @@ export default class SignUpForm extends Component {
     return (
       <form onSubmit={this.props.onCreateAccount}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" placeholder="username" />
+        <input required type="text" id="username" name="username" placeholder="username" />
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="email" />
+        <input required type="email" id="email" name="email" placeholder="email" />
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="password" />
+        <input required type="password" id="password" name="password" placeholder="password" />
         <button>Create Account</button>
       </form>
     );
